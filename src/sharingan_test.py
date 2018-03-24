ffb14d9963229cf241250d21811acdf25e7fbd25
Nobuhiro Kuroiwa <nobu@falcon.local>
2018-03-25 02:39:57 +0900

    implement testing script

--- src/sharingan_test.py
+++ src/sharingan_test.py
@@ -8,24 +8,17 @@ import argparse
 import os
 import inspect
 import json
-import random
 from sharingan_base import *
 
 def processArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", help="path to folder containing images")
     parser.add_argument("--output_dir", required=True, help="where to put output files")
-    parser.add_argument("--checkpoint", default=None, help="directory with checkpoint to resume training from or use for testing")
+    parser.add_argument("--checkpoint", required=True, help="directory with checkpoint to use for testing")
     parser.add_argument("--max_steps", type=int, help="number of training steps (0 to disable)")
-    parser.add_argument("--max_epochs", type=int, help="number of training epochs")
     parser.add_argument("--batch_size", type=int, default=1, help="number of images in batch")
 
     a = parser.parse_args()
-
-    seed = 0
-    tf.set_random_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
 
     if not os.path.exists(a.output_dir):
         os.makedirs(a.output_dir)
@@ -40,7 +33,7 @@ def main():
 
     processArgs()
 
-    examples = load_examples(input_dir=a.input_dir, batch_size=a.batch_size, is_training=True)
+    examples = load_examples(input_dir=a.input_dir, batch_size=a.batch_size, is_training=False)
     print("examples count = %d" % examples.count)
 
     # inputs and targets are [batch_size, height, width, channels]
@@ -58,16 +51,6 @@ def main():
             "outputs": outputs,
         }
 
-    tf.summary.scalar("discriminator_loss", model.discrim_loss)
-    tf.summary.scalar("generator_loss_GAN", model.gen_loss_GAN)
-    tf.summary.scalar("generator_loss_L1", model.gen_loss_L1)
-
-    for var in tf.trainable_variables():
-        tf.summary.histogram(var.op.name + "/values", var)
-
-    for grad, var in model.discrim_grads_and_vars + model.gen_grads_and_vars:
-        tf.summary.histogram(var.op.name + "/gradients", grad)
-
     with tf.name_scope("parameter_count"):
         parameter_count = tf.reduce_sum([tf.reduce_prod(tf.shape(v)) for v in tf.trainable_variables()])
 
@@ -81,16 +64,10 @@ def main():
     }
     logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=progress_freq)
 
-    max_steps = examples.steps_per_epoch * a.max_epochs
-    summary_op = tf.summary.merge_all()
+    max_steps = examples.steps_per_epoch
 
     hooks = [
         tf.train.StopAtStepHook(num_steps=max_steps),
-        tf.train.CheckpointSaverHook(save_steps=save_freq,checkpoint_dir=a.output_dir,saver=saver),
-        tf.train.SummarySaverHook(save_steps=summary_freq, summary_op=summary_op),
-        logging_hook,
-        tf.train.StepCounterHook(every_n_steps=progress_freq),
-        SaveImageHook(output_dir=a.output_dir, fetches=display_fetches, save_steps=save_freq)
     ]
 
     global_step = tf.train.get_or_create_global_step()
@@ -103,13 +80,20 @@ def main():
                                        config=tf.ConfigProto(allow_soft_placement=True),
                                        is_chief=True,
                                        scaffold = scaffold,
-                                       hooks=hooks) as sess:    
+                                       hooks=hooks) as sess:
+        print("loading model from checkpoint")
+        checkpoint = tf.train.latest_checkpoint(a.checkpoint)
+        saver.restore(sess, checkpoint)
+
         coord = tf.train.Coordinator()
 
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
         try:
+            counter = 0
             while not sess.should_stop():
-                out = sess.run(model)
+                fetches = sess.run(display_fetches)
+                save_images(a.output_dir, fetches, counter)
+                counter = counter + 1
         except tf.errors.OutOfRangeError:
             print('Done training -- epoch limit reached')
         finally:
