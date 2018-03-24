f2103d7a4114802bb9fd14a2fe1e2add390eb7ae
Nobuhiro Kuroiwa <nobu@falcon.local>
2018-03-24 21:41:45 +0900

    fix typo

---
+++ src/sharingan_test.sh
@@ -0,0 +1,7 @@
+
+python sharingan_test.py \
+  --mode test \
+  --output_dir "../data/sharingan_test" \
+  --input_dir "../data/input/evaluation" \
+  --checkpoint sharingan_checkpoints
+  
---
+++ src/sharingan_train.sh
@@ -0,0 +1,5 @@
+
+python sharingan_train.py \
+  --output_dir sharingan_checkpoints \
+  --max_epochs 200 \
+  --input_dir "../data/input/training"
