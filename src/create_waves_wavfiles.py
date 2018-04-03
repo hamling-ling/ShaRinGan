d164211e83f1e07124b56c693d3567aecec36da6
Nobuhiro Kuroiwa <nobu@falcon.local>
2018-04-03 21:45:19 +0900

    add script to make training input file from guitar wav wfiles

---
+++ src/create_waves_wavfiles.py
@@ -0,0 +1,53 @@
+import numpy as np
+import soundfile as sf
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import os
+
+sz=1024
+
+training_fn_pairs = [
+    ["../data/raw_waves/gt_notes.wav", "../data/raw_waves/gt_notes_dist.wav"],
+    ["../data/raw_waves/gt_many_notes.wav", "../data/raw_waves/gt_many_notes_dist.wav"]
+]
+
+def readWave(path_src, path_cnv):
+    data_src, samplerate_src = sf.read(path_src)
+    data_cnv, samplerate_cnv = sf.read(path_cnv)
+    data_len = min(data_src.shape[0], data_cnv.shape[0])
+    print("read ", path_src, " and ", path_cnv)
+    print("len=", data_len)
+    data_src = np.array(data_src)
+    data_cnv = np.array(data_cnv)
+    data_src_trim = data_src[:data_len]
+    data_cnv_trim = data_cnv[:data_len]
+    return data_src_trim, data_cnv_trim
+
+def outputWave(input_fn_pairs, output_path):
+    file_counter = 0
+    for fnp in input_fn_pairs:
+        data_src, data_cnv  = readWave(fnp[0], fnp[1])
+        loops = int(len(data_src)/sz)
+        for i in np.arange(loops):
+            data = np.zeros([2,sz], dtype=np.float32)
+            i_from = i * sz
+            data[0] = data_src[i_from:i_from+sz]
+            data[1] = data_cnv[i_from:i_from+sz]
+            fn = os.path.join(output_path, "{0:0>4}.bin".format(file_counter))
+            data.tofile(fn)
+            print(fn, " saved")
+            file_counter = file_counter + 1
+
+out_train="../data/input/training"
+out_validation = "../data/input/validation"
+out_evaluation = "../data/input/evaluation"
+
+os.makedirs("../data", exist_ok=True)
+os.makedirs("../data/input", exist_ok=True)
+os.makedirs(out_train, exist_ok=True)
+os.makedirs(out_validation, exist_ok=True)
+os.makedirs(out_evaluation, exist_ok=True)
+
+outputWave(training_fn_pairs, out_train)
+
