0e0d4c30937fcc9e17998248d1ec70916db0a1e8
hamling-ling <tacobell@gj9.so-net.ne.jp>
2018-05-10 00:26:15 +0900

    make training resumable from specified checkpoint

--- src/sharingan_train.sh
+++ src/sharingan_train.sh
@@ -1,5 +1,7 @@
 
 python sharingan_train.py \
-  --output_dir sharingan_checkpoints \
+  --output_dir "sharingan_checkpoints" \
   --max_epochs 200 \
-  --input_dir "../data/input/training"
+  --input_dir "../data/input/training" \
+  --checkpoint "./sharingan_checkpoints"
+
