872ec6e2a7bc54f145203bd30d946ae72315a128
Nobuhiro Kuroiwa <nobubijinesu+github@gmail.com>
2018-12-11 01:38:23 +0000

    add continue parameter to training script

--- src/sharingan_train.sh
+++ src/sharingan_train.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+CHECK_POINT_OPT=
+if [ "$1" == "--continue" ]; then
+    CHECK_POINT_OPT="--checkpoint ./sharingan_checkpoints"
+fi
+echo CHECK_POINT_OPT=$INPUT_DIR_OPT
 
 python sharingan_train.py \
   --output_dir "sharingan_checkpoints" \
   --max_epochs 200 \
-  --input_dir "../data/input/training" \
-  --checkpoint "./sharingan_checkpoints"
+  --input_dir "../data/input/training" $CHECK_POINT_OPT
+
+
 
