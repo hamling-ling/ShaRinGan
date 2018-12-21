f5b2bfb0b8298328e4c5b36fbc66cbd82916c417
Nobuhiro Kuroiwa <nobubijinesu+github@gmail.com>
2018-12-21 15:09:29 +0000

    make 10 times more date from single wav file

--- src/create_waves_wavfiles.py
+++ src/create_waves_wavfiles.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os
 
 sz=1024
+offsets = np.array([0, 100, 200, 300, 400, 500, 600, 700, 800, 900])
 
 training_fn_pairs = [
     ["../data/raw_waves/gt_notes.wav", "../data/raw_waves/gt_notes_dist.wav"],
@@ -32,16 +33,17 @@ def outputWave(input_fn_pairs, output_path):
     file_counter = 0
     for fnp in input_fn_pairs:
         data_src, data_cnv  = readWave(fnp[0], fnp[1])
-        loops = int(len(data_src)/sz)
+        loops = int(len(data_src)/sz) - 1
         for i in np.arange(loops):
-            data = np.zeros([2,sz], dtype=np.float32)
-            i_from = i * sz
-            data[0] = data_src[i_from:i_from+sz]
-            data[1] = data_cnv[i_from:i_from+sz]
-            fn = os.path.join(output_path, "{0:0>4}.bin".format(file_counter))
-            data.tofile(fn)
-            print(fn, " saved")
-            file_counter = file_counter + 1
+            for j in np.arange(offsets.shape[0]):
+                data = np.zeros([2,sz], dtype=np.float32)
+                i_from = i * sz + offsets[j]
+                data[0] = data_src[i_from:i_from+sz]
+                data[1] = data_cnv[i_from:i_from+sz]
+                fn = os.path.join(output_path, "{0:0>6}.bin".format(file_counter))
+                data.tofile(fn)
+                print(fn, " saved")
+                file_counter = file_counter + 1
 
 out_train="../data/input/training"
 out_validation = "../data/input/validation"
