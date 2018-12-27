5c1760233f54110c462246bcb217080014089361
Nobuhiro Kuroiwa <nobubijinesu+github@gmail.com>
2018-12-27 10:28:39 +0000

    fix wrong output path for sharingan_test.sh

--- src/sharingan_test.sh
+++ src/sharingan_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python sharingan_test.py \
-  --output_dir "../data/cvt/pretrained_model" \
+  --output_dir "../data/test/pretrained_model" \
   --input_dir "../data/input/evaluation" \
   --max_steps 100000 \
   --checkpoint "../data/output/pretrained_model/model.ckpt-8000"
