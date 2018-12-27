55b6ef2fd512ea9694b0a464f191673f5f31a6aa
Nobuhiro Kuroiwa <nobubijinesu+github@gmail.com>
2018-12-27 10:15:14 +0000

    set sharingan_test parameter for pre-trained model

--- src/sharingan_test.sh
+++ src/sharingan_test.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
 python sharingan_test.py \
-  --output_dir "../data/sharingan_test" \
+  --output_dir "../data/cvt/pretrained_model" \
   --input_dir "../data/input/evaluation" \
-  --max_steps 100 \
-  --checkpoint sharingan_checkpoints
+  --max_steps 100000 \
+  --checkpoint "../data/output/pretrained_model/model.ckpt-8000"
   
