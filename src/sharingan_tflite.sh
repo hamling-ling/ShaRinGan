d6485658929e44d5c9b55001145c6df6d7e31fd5
Nobuhiro Kuroiwa <nobubijinesu+github@gmail.com>
2019-02-09 17:06:57 +0000

    add tflite exporting script

---
+++ src/sharingan_tflite.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+toco \
+    --graph_def_file=../data/cvt/frozen_model.pb \
+    --output_file=../data/cvt/model.tflite \
+    --output_format=TFLITE \
+    --input_shae=1,1,1024,1 \
+    --input_array=input \
+    --output_array=generator/Tanh \
+    --inference_type=FLOAT \
+    --input_data_type=FLOAT
