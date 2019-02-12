f6f9b3f1ab259390a21889809f3a050dca5665e9
hamling-ling <nobubijinesu@gmail.com>
2019-02-13 01:31:57 +0900

    change path to output frozen model

--- src/sharingan_tflite.sh
+++ src/sharingan_tflite.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 tflite_convert \
-    --graph_def_file=../data/cvt/frozen_model.pb \
-    --output_file=../data/cvt/model.tflite \
+    --graph_def_file=../data/output/frozen_model/frozen_model.pb \
+    --output_file=../data/output/frozen_model/model.tflite \
     --output_format=TFLITE \
     --input_shae=1,1,1024,1 \
     --input_array=input \
