b7b9d455497dbc8d87763b09a637ff3f57bd76da
hamling-ling <nobubijinesu@gmail.com>
2019-03-08 23:51:05 +0900

    save console output

--- src/sharingan_train.sh
+++ src/sharingan_train.sh
@@ -6,14 +6,14 @@ if [ "$1" == "--continue" ]; then
 fi
 echo CHECK_POINT_OPT=$INPUT_DIR_OPT
 
+OUTPUT_DIR="../data/output/pretrained_model"
+rm -rf $OUTPUT_DIR
+
 python sharingan_train.py \
-  --output_dir "../data/output/pretrained_model" \
+  --output_dir $OUTPUT_DIR \
   --max_epochs 200 \
   --lr 0.001 \
   --ngf 32 \
   --ndf 32 \
   --batch_size 512 \
-  --input_dir "../data/input/training"
-
-
-
+  --input_dir "../data/input/training" | 2>&1 tee $OUTPUT_DIR/output.log
