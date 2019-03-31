80381a1fbf9ece0ee8fcd08070aba7d3a253a1e1
hamling-ling <nobubijinesu@gmail.com>
2019-03-31 17:27:37 +0900

    make quantization graph exportable but commented out since concat op not support min/max info and  not work

--- src/sharingan_export.py
+++ src/sharingan_export.py
@@ -10,6 +10,7 @@ import inspect
 import json
 from sharingan_base import *
 from collections import namedtuple
+from tensorflow.python.platform import gfile
 
 SZ = 1024
 
@@ -45,12 +46,7 @@ def save_last_node_name(node_name):
 def main():
     a, hyper_params = process_args()
 
-    tf.reset_default_graph()
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(tf.local_variables_initializer())
-
+    with tf.Graph().as_default() as graph:
         input = tf.placeholder("float", [1, 1, SZ, 1], name="input")
         with tf.variable_scope("generator"):
             generator = create_generator(generator_inputs           = input,
@@ -58,49 +54,19 @@ def main():
                                          hyper_params               = hyper_params,
                                          is_training                = False,
                                          is_fused                   = False)
-            last_node_name=generator.name.split(":")[0]
-            save_last_node_name(generator.name.split(":")[0])
 
-        saver = tf.train.Saver(tf.global_variables())
-        print("saver created")
-
-        print("loading model from checkpoint")
-        if os.path.isdir(a.checkpoint):
-            print("restoring latest in ", a.checkpoint)
-            checkpoint = tf.train.latest_checkpoint(a.checkpoint)
-            saver.restore(sess, checkpoint)
+        # save graph that not including weights
+        filename = "graph.pb"
+        if(hyper_params.enable_quantization):
+            print("exporting quantized graph")
+            tf.contrib.quantize.create_eval_graph()
+            filename = "quantized_graph.pb"
         else:
-            print("restoring from a file ", a.checkpoint)
-            saver.restore(sess, a.checkpoint)
-        print("restored")
+            print("exporting on-quantized graph")
 
-        outfile = os.path.join(a.output_dir, "movidius")
-        saver.save(sess, outfile)
-        print("saved ", outfile)
-
-        # We use a built-in TF helper to export variables to constants
-        output_graph_def = tf.graph_util.convert_variables_to_constants(
-            sess, # The session is used to retrieve the weights
-            tf.get_default_graph().as_graph_def(), # The graph_def is used to retrieve the nodes 
-            [last_node_name]
-        ) 
-
-    # Finally we serialize and dump the output graph to the filesystem
-    frozen_model_name=os.path.join(a.output_dir, "frozen_model.pb")
-    with tf.gfile.GFile(frozen_model_name, "wb") as f:
-        f.write(output_graph_def.SerializeToString())
-    print("%d ops in the final graph." % len(output_graph_def.node))
-
-    # save
-    tflite_name = os.path.join(a.output_dir, "model.tflite")
-    converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(
-        graph_def_file=frozen_model_name,
-        input_arrays=["input"],
-        output_arrays=["generator/Tanh"])
-    tflite_model = converter.convert()
-    open(tflite_name, "wb").write(tflite_model)
-
-    col = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='generator')
-    for item in col:
-        print(item)
+        graph_def = graph.as_graph_def()
+        outfile = os.path.join(a.output_dir, filename)
+        with gfile.GFile(outfile, 'wb') as f:
+            f.write(graph_def.SerializeToString())
+        print(outfile, "saved")
 main()
--- src/sharingan_export.sh
+++ src/sharingan_export.sh
@@ -1,6 +1,62 @@
 #!/bin/bash
 
+# Where the chepoint file is.
+CHEKPOINT_FILE="../data/output/quantization_aware_model/model.ckpt-95850"
+
+echo -------- exporting --------
 rm -rf ../data/output/frozen_model
-python sharingan_export.py \
+python sharingan_export_test.py \
   --output_dir "../data/output/frozen_model" \
-  --checkpoint "../data/output/pretrained_model/model.ckpt-7001"
+  --checkpoint $CHEKPOINT_FILE
+
+echo -------- freezing --------
+python -m tensorflow.python.tools.freeze_graph \
+--input_graph="../data/output/frozen_model/inference_graph.pb" \
+--input_checkpoint=${CHEKPOINT_FILE} \
+--input_binary=true \
+--output_graph="../data/output/frozen_model/frozen.pb" \
+--output_node_names=generator/Tanh
+
+echo -------- converting to tflite --------
+convert_to_fp_tflite()
+{
+  FORMAT=$1
+  OUT_FILE=$2
+  echo converting $FORMAT from $OUT_FILE
+  tflite_convert --graph_def_file="../data/output/frozen_model/frozen.pb" \
+  --output_file=$OUT_FILE \
+  --input_format=TENSORFLOW_GRAPHDEF \
+  --output_format=$FORMAT \
+  --inference_type=FLOAT \
+  --output_arrays=generator/Tanh \
+  --input_arrays=input \
+  --mean_values 121 \
+  --std_dev_values=64
+}
+
+convert_to_ui8_tflite()
+{
+  FORMAT=$1
+  OUT_FILE=$2
+  echo converting $FORMAT from $OUT_FILE
+  tflite_convert --graph_def_file="../data/output/frozen_model/frozen.pb" \
+  --output_file=$OUT_FILE \
+  --input_format=TENSORFLOW_GRAPHDEF \
+  --output_format=$FORMAT \
+  --inference_type=QUANTIZED_UINT8 \
+  --output_arrays=generator/Tanh \
+  --input_arrays=input \
+  --mean_values 121 \
+  --std_dev_values=64 \
+  --default_ranges_min=0 \
+  --default_ranges_max=6
+}
+
+#convert_to_ui8_tflite TFLITE ../data/output/frozen_model/sharingan_ui8.tflite
+#convert_to_ui8_tflite GRAPHVIZ_DOT "../data/output/frozen_model/sharingan_ui8.dot"
+
+convert_to_fp_tflite TFLITE ../data/output/frozen_model/sharingan_fp.tflite
+convert_to_fp_tflite GRAPHVIZ_DOT "../data/output/frozen_model/sharingan_fp.dot"
+
+#how to convert
+#dot -Tpdf -O /tmp/foo.dot
