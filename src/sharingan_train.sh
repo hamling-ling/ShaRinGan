7d5eb3b93779b6c4868656b62971838f93b5b5ad
hamling-ling <nobubijinesu@gmail.com>
2019-03-31 17:55:24 +0900

    adjust parameter

--- src/sharingan_train.sh
+++ src/sharingan_train.sh
@@ -8,12 +8,15 @@ echo CHECK_POINT_OPT=$INPUT_DIR_OPT
 
 OUTPUT_DIR="../data/output/pretrained_model"
 rm -rf $OUTPUT_DIR
+mkdir $OUTPUT_DIR
 
 python sharingan_train.py \
   --output_dir $OUTPUT_DIR \
-  --max_epochs 200 \
-  --lr 0.001 \
+  --max_epochs 150 \
+  --lr 0.00004 \
+  --beta1 0.5 \
+  --conv_std 0.0005 \
   --ngf 32 \
   --ndf 32 \
-  --batch_size 512 \
-  --input_dir "../data/input/training" | 2>&1 tee $OUTPUT_DIR/output.log
+  --batch_size 32 \
+  --input_dir "../data/input/training" 2>&1 | tee $OUTPUT_DIR/output.log
