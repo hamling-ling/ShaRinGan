3366391d8eaf2073541656ca3504bf470fd95d70
hamling-ling <nobubijinesu@gmail.com>
2019-05-06 05:06:06 +0000

    change download file linke becaulse downloading bigfile from google drive requires something specifal technique.

--- data/output/download_pretrained.sh
+++ data/output/download_pretrained.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o pretrained_model-1.3.zip "https://docs.google.com/uc?export=download&id=1k-Mpsnrn5SC8zvVsT-Q2wjClprQspa-0"
+curl -L -O https://github.com/hamling-ling/ShaRinGan/releases/download/pretrained1.3/pretrained_model-1.3.zip
 curl -L -O https://github.com/hamling-ling/ShaRinGan/releases/download/pretrained1.2/pretrained_model-1.2-quantized.zip
 
 #for non-quantized model
