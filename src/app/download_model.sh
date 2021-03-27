1c5fb1bf482af93c8ff15802b5fc93d3b976c25e
Nobuhiro Kuroiwa <nobubijinesu@gmail.com>
2021-03-27 15:14:16 +0900

    update frozen model download file

--- src/app/download_model.sh
+++ src/app/download_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L -O https://github.com/hamling-ling/ShaRinGan/releases/download/pretrained1.3/frozen_model-1.3.zip
-unzip frozen_model-1.3.zip
+curl -L -O https://github.com/hamling-ling/ShaRinGan/releases/download/pretrained1.4/frozen_model-1.4.zip
+unzip frozen_model-1.4.zip
