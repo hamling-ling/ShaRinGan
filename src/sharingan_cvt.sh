650798717f55fb79507ffa41d77bc993d3e0f3ab
Nobuhiro Kuroiwa <nobubijinesu@gmail.com>
2021-03-27 14:25:31 +0900

    update checkpoint file name since parameter changed

--- src/sharingan_cvt.sh
+++ src/sharingan_cvt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+CHECKPOINT_DIR=../data/output/pretrained_model
+CHEKPOINT_FILE=$CHECKPOINT_DIR/model.ckpt-162000
+
 python sharingan_cvt.py \
   --output_dir "../data/cvt/pretrained_model" \
   --input_dir "../data/input/evaluation" \
   --max_steps 100000 \
-  --checkpoint "../data/output/pretrained_model/model.ckpt-33000"
+  --checkpoint "$CHEKPOINT_FILE"
